6beee3917db2e37b601a14c07981c2e2854dfd96
Anh Duc <ladcva@yahoo.com>
2019-12-29 15:11:09 +0700

    Initial Commit

---
+++ c17/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c17
+//
+//  Created by Anh Duc Le on 12/29/19.
+//  Copyright © 2019 Anh Duc Le. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
